b7754d9633d5063cb15a639373083e5c26e307a2
Guilherme Gimenez <guilhermegimenez385@gmail.com>
2026-05-28 20:39:16 -0300

    Criando o funcionamento do slideshow

--- js/script.js
+++ js/script.js
@@ -13,3 +13,80 @@ window.addEventListener("scroll", () => {
     header.style.boxShadow = "none";
   }
 });
+
+// SLIDESHOW
+const slides = [
+  {
+    image: "./img/satelite.jpg",
+    title: "Monitoramento Orbital",
+    description:
+      "Satélites analisam grandes áreas em tempo real para identificar riscos ambientais.",
+  },
+  {
+    image: "./img/problema.jpg",
+    title: "Detecção de Focos de Calor",
+    description:
+      "O sistema identifica regiões com risco elevado antes que o incêndio se espalhe.",
+  },
+  {
+    image: "./img/tecnologia.jpg",
+    title: "Análise Inteligente de Dados",
+    description:
+      "A inteligência artificial interpreta padrões térmicos e apoia decisões preventivas.",
+  },
+];
+
+let currentSlide = 0;
+
+const slideImage = document.querySelector("#slideImage");
+const slideTitle = document.querySelector("#slideTitle");
+const slideDescription = document.querySelector("#slideDescription");
+const nextSlide = document.querySelector("#nextSlide");
+const prevSlide = document.querySelector("#prevSlide");
+const dots = document.querySelectorAll(".dot");
+
+function updateSlide(index) {
+  slideImage.src = slides[index].image;
+  slideImage.alt = slides[index].title;
+
+  slideTitle.textContent = slides[index].title;
+  slideDescription.textContent = slides[index].description;
+
+  dots.forEach((dot) => {
+    dot.classList.remove("active-dot");
+  });
+
+  dots[index].classList.add("active-dot");
+}
+
+function showNextSlide() {
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  updateSlide(currentSlide);
+}
+
+function showPrevSlide() {
+  currentSlide--;
+
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+
+  updateSlide(currentSlide);
+}
+
+nextSlide.addEventListener("click", showNextSlide);
+prevSlide.addEventListener("click", showPrevSlide);
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    currentSlide = index;
+    updateSlide(currentSlide);
+  });
+});
+
+setInterval(showNextSlide, 5000);
